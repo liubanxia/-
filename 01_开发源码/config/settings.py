18705f84217e54fa6671939bb2417895075c92f4
Project Phoenix <phoenix@local.invalid>
2026-08-06 21:19:51 +0800

    完成项目基础框架

---
+++ 01_开发源码/config/settings.py
@@ -0,0 +1,20 @@
+"""
+Project Phoenix 配置文件
+"""
+
+# 项目名称
+PROJECT_NAME = "Project Phoenix"
+
+# 当前版本
+VERSION = "V1.0.0"
+
+# 开发模式
+DEBUG = True
+
+# 支持影像类型
+SUPPORTED_MODALITIES = [
+    "CT",
+    "MR",
+    "CR",
+    "DR"
+]
