2b4fcd24c4e3ab61add2a7f74f39feb6431996d6
liubanxia <liubanxia250@gmail.com>
2026-08-19 21:32:22 +0800

    Install translation output recovery hook

--- 14_学习中心/离线医学知识工作台/phoenix_knowledge/__init__.py
+++ 14_学习中心/离线医学知识工作台/phoenix_knowledge/__init__.py
@@ -1,3 +1,6 @@
 from .workbench import MedicalKnowledgeWorkbench
+from .translation_recovery import install as _install_translation_recovery
+
+_install_translation_recovery()
 
 __all__ = ["MedicalKnowledgeWorkbench"]
