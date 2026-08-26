313bee7214fa4e4d5a5871d3a9c374cc56a014dc
liubanxia <liubanxia250@gmail.com>
2026-08-26 15:01:10 +0800

    Add AZ3 to map learning catalog

--- ios/PhoenixRealtimeVisionAssist/Sources/Map/DeltaMapCatalog.swift
+++ ios/PhoenixRealtimeVisionAssist/Sources/Map/DeltaMapCatalog.swift
@@ -10,8 +10,9 @@ enum DeltaMapCatalog {
     static let all: [MapCatalogEntry] = [
         MapCatalogEntry(id: .zeroDam, displayName: "Zero Dam / 零号大坝", learningPriority: 1),
         MapCatalogEntry(id: .spaceCity, displayName: "Space City / 航天基地", learningPriority: 2),
-        MapCatalogEntry(id: .layaliGrove, displayName: "Layali Grove / 长弓溪谷", learningPriority: 3),
-        MapCatalogEntry(id: .brakkesh, displayName: "Brakkesh / 巴克什", learningPriority: 4),
-        MapCatalogEntry(id: .tidePrison, displayName: "Tide Prison / 潮汐监狱", learningPriority: 5)
+        MapCatalogEntry(id: .az3, displayName: "AZ3", learningPriority: 3),
+        MapCatalogEntry(id: .layaliGrove, displayName: "Layali Grove / 长弓溪谷", learningPriority: 4),
+        MapCatalogEntry(id: .brakkesh, displayName: "Brakkesh / 巴克什", learningPriority: 5),
+        MapCatalogEntry(id: .tidePrison, displayName: "Tide Prison / 潮汐监狱", learningPriority: 6)
     ]
 }
